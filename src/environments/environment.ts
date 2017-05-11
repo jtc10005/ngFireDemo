2ab7394eafeaa80a94561a70cf096b40dfa701a0
jtc10005 <jtc10005@gmail.com>
2017-05-10 22:51:57 -0500

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,19 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// export const environment = {
+//   production: false
+// };
+
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyACtk_VX61i_w_0B0YpKlL2x66OLCNgv3s',
+    authDomain: 'ngfire-e9afc.firebaseapp.com',
+    databaseURL: 'https://ngfire-e9afc.firebaseio.com',
+    projectId: 'ngfire-e9afc',
+    storageBucket: 'ngfire-e9afc.appspot.com',
+    messagingSenderId: '649810772121'
+  }
 };
