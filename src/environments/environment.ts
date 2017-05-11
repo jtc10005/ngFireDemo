38894f99f8a2e03c2507a7f3dc185d8f01934b96
jtc10005 <jtc10005@gmail.com>
2017-05-11 11:51:32 -0500

    update readme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,11 +11,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyACtk_VX61i_w_0B0YpKlL2x66OLCNgv3s',
-    authDomain: 'ngfire-e9afc.firebaseapp.com',
-    databaseURL: 'https://ngfire-e9afc.firebaseio.com',
-    projectId: 'ngfire-e9afc',
-    storageBucket: 'ngfire-e9afc.appspot.com',
-    messagingSenderId: '649810772121'
+...
   }
 };
