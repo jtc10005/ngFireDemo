b71f5aa30cba8d15204231443b473938d9bb45a6
Jay Culpepper <jay.culpepper@ldcom.com>
2017-05-23 11:48:33 -0500

    themes working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { PeopleComponent } from './people/people.component';
+import { MaterialModule } from '@angular/material';
 import { SharedModule } from './shared/shared.module';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MdButtonModule, MdCheckboxModule, MdGridListModule, MdToolbarModule, MdInputModule, MdListModule } from '@angular/material';
+import {
+  MdButtonModule,
+  MdCheckboxModule,
+  MdGridListModule,
+  MdToolbarModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MdButtonModule, MdCheckboxModule, MdGridListModule, MdInputModule, MdButtonModule, MdListModule],
-  exports: [MdButtonModule, MdCheckboxModule, MdGridListModule, MdInputModule, MdButtonModule, MdListModule],
+  imports: [
+    MdButtonModule,
+    MdCheckboxModule,
+    MdGridListModule,
+    MdInputModule,
+    MdButtonModule,
+    MdListModule,
+    MdMenuModule,
+    MdToolbarModule
+  ],
+  exports: [
+    MdButtonModule,
+    MdCheckboxModule,
+    MdGridListModule,
+    MdInputModule,
+    MdButtonModule,
+    MdListModule,
+    MdMenuModule,
+    MdToolbarModule
+  ],
   declarations: []
 })
 export class SharedModule { }
